982a13ea082b134d0a67ae5f28585d48a5bf843d
phillipcarvalho <145431348+phillipcarvalho@users.noreply.github.com>
2024-07-22 11:32:54 +0530

    22-7-24

---
+++ labwork2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class labwork2 {
+    public static void main(String args[]){
+       String firstname="John";
+       String lastname="Doe";
+       System.out.println(firstname+" "+ lastname);
+       System.out.println(firstname.concat(lastname));
+        String x="10";
+        int y=20;
+        String z=x+y;
+        System.out.println(z);
+        String txt="We are the so called \"Vikings\" from the North";
+        System.out.println(txt);
+        System.out.println(Math.max(5,10));
+        System.out.println(Math.min(5,10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.random());
+    }
+}
